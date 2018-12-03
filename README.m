72eb904752ce6465bbd114e09c741c9ed6fe8be0
KevinMena <kevin_mena3@hotmail.com>
2018-12-02 22:37:38 -0400

    Maquinas de Estado y Readme

--- README.m
+++ README.m
@@ -1,1 +1,14 @@
 "# Inteligencia-Para-Juegos" 
+
+Hola Chang, para acceder al codigo del juego debes:
+
+1) Entrar en la carpeta "IAJ"
+2) Entrar en la carpeta "Assets"
+3) Aqui encontraras el codigo de las maquinas de estado
+4) Entrar en la carpeta "Scripts"
+5) Aca se encuentran el resto de codigos del juego.
+
+
+No te he tenido chance de hacer una build para que sea medio jugable o mejor dicho probable, lo hare en estos dias para que lo pueda probar usted misma.
+
+Saludos
